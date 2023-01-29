a30af1de69f232dbb8b721de62c357824a479c38
Mary Lueder <mlueder@gmail.com>
2023-01-29 13:42:16 -0500

    add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
+import Resume from './pages/Resume';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/contact' element={<Contact />}/>
+        <Route path='/projects' element={<Projects />}/>
+        <Route path='/resume' element={<Resume />}/>
+      </Routes>
+    </>
   );
 }
 
