dfffcd3e71bd70935552e3b12454c71265f54c83
Mary Lueder <mlueder@gmail.com>
2023-01-30 17:13:54 -0500

    add project data and screenshots

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 icons https://icons8.com
 
-border gradient https://codepen.io/GeorgePark/pen/EEGJEj
+(No longer in use?) border gradient idea https://codepen.io/GeorgePark/pen/EEGJEj
+
+gold gradient http://www.brandgradients.com/gold-gradient/
 
 background https://unsplash.com/photos/pKpipZVVEC0
 
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,16 @@
+import { projects } from "../data/projects"
+
 const Projects = () => {
   return (
     <>
       <h1>Projects</h1>
+      <ul>
+        {projects.map(project => (
+          <li key={project.title}>
+            {project.title}
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
