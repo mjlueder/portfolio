0e4f5c8f28d2397f6618cea16c3fbfd18d66dbaa
Mary Lueder <mlueder@gmail.com>
2023-02-01 21:03:53 -0500

    add resume, adjust sizing

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,13 +6,13 @@ const About = () => {
     <>
       <h1>About Mary</h1>
       <img src={photo} alt="profile" id="main-photo"/>
-      <p>I'm Mary, a full stack engineer with more than a decade of experience in conservation, animal care, and ecology. I try to follow my heart and my passion, and my career has taken me from ecology field work, to sea turtle rehabilitation, to animal behavior studies, and then onto the primate team at Disney’s Animal Kingdom.</p> 
+      <p>I'm Mary, a full stack engineer with more than a decade of experience in conservation, animal care, and ecology. I try to follow my heart and my passion, and my career has taken me from ecology field work, to sea turtle rehabilitation, and then onto the primate team at Disney’s Animal Kingdom.</p> 
 
-      <p>I’ve had the opportunity to collaborate with many teams and lines of business across The Walt Disney Company. I love connecting with and bringing out the best in others. I have grown a lot as a creative problem-solver through the unique challenges and opportunities of providing excellent care for many different endangered species.</p>
+      <p>I had the opportunity to collaborate with many teams and lines of business across The Walt Disney Company. I love connecting with and bringing out the best in others, and I look forward to bringing that into my future roles. I also gained a lot of experience as a creative problem-solver through the unique challenges and opportunities of providing excellent care for many different endangered species. </p>
 
-      <p>I love the creativity and logic of programming. I enjoy the balance between backend functionality and frontend aesthetics and flow. I’m excited to take my unique skills and experiences and apply them to new problems. Ultimately, I want to continue to do work that is doing good; I think that’s incredibly important.</p>
+      <p>I love the creativity and logic of programming. So far, I enjoy both backend and frontend work, and merging functionality, aesthetics, and flow. I’m excited to take my unique skills and experiences and apply them to new problems with new teams. Ultimately, I want to continue to do work that is doing good; I think that’s incredibly important.</p>
 
-      <p>*ADD DOG PICTURES!* Outside of work I also love music, yoga, mindfulness, wildlife, psychology, health, and wellness, and training my two crazy but wonderful dogs.</p>
+      <p id="last-p">Outside of work I also love music, yoga, mindfulness, wildlife, psychology, health, and wellness, and training my two crazy but wonderful dogs.</p>
     </>
   )
 }
--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -14,19 +14,21 @@ const ProjectDetails = () => {
     <>
       <h1>{project[0].title}</h1>
       <img src={project[0].image} alt={project[0].title} className='details-pic'/>
-      <p>{project[0].description1}</p>
-      <p>{project[0].description2}</p>
+      <div className='description'>
+        <p>{project[0].description1}</p>
+        <p>{project[0].description2}</p>
+      </div>
       <div className='project-btns'>
+        <Link to={project[0].deployLink} target='_blank'>
+          <button className='project-btn'>Deployed App</button>
+        </Link>
         <Link to={project[0].repoLink} target='_blank'>
           <button className='project-btn'>Repository</button>
         </Link>
-        <Link to={project[0].deployLink} target='_blank'>
-          <button className='project-btn'>Deployed!</button>
+        <Link to='/projects' className='back-btn project-btn'>
+          <button>Back</button>
         </Link>
       </div>
-      <Link to='/projects'>
-        <button>Back</button>
-      </Link>
     </>
   )
 }
